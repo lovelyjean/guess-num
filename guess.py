85556b80a2f34d2eae543b617df445efb4656b0e
lovelyjean <lovely.jean@gmail.com>
2020-12-30 18:34:25 +0900

    add count

--- guess.py
+++ guess.py
@@ -1,12 +1,15 @@
 import random
 
 r = random.randint(1, 100)
+cnt = 0
 
 while True:
 	num = input('please guess a number: ')
 	num = int(num)
+	cnt = cnt + 1
 	if num == r:
 		print('Correct!')
+		print('you have guessed ', cnt , 'times')
 		break
 	else:
 		if num > r:
